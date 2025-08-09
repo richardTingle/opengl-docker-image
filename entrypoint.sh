166818eec1794bd91c4f39d28c92d5c0f911585d
Richard Tingle <6330028+richardTingle@users.noreply.github.com>
2025-08-09 09:19:59 +0100

    Stop starting the server in the entrypoint, it doesn't seem to work

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,4 @@ mkdir -p /tmp/runtime-dir
 chmod 700 /tmp/runtime-dir
 export XDG_RUNTIME_DIR=/tmp/runtime-dir
 
-# Start virtual X
-Xvfb :99 -ac -screen 0 1024x768x24 &
-sleep 1
-
-# Make DISPLAY visible to subsequent GHA steps too
-if [ -n "${GITHUB_ENV:-}" ]; then
-  echo "DISPLAY=${DISPLAY}" >> "$GITHUB_ENV"
-fi
-
 exec "$@"
